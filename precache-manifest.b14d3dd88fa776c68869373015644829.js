5132f9c52f7108ad9c74828c2d95b1c62cd4c144
gufran <gufranoist@gmail.com>
2020-09-03 02:18:06 +0530

    Updates

---
+++ precache-manifest.b14d3dd88fa776c68869373015644829.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614f3985cda580676e85bffee7d5c6b9",
+    "url": "/rashidtruba/react-todo/index.html"
+  },
+  {
+    "revision": "90a37e2c87abcf3f9455",
+    "url": "/rashidtruba/react-todo/static/css/main.0e5a2f6f.chunk.css"
+  },
+  {
+    "revision": "3fa6d5a02ddc0c58f0da",
+    "url": "/rashidtruba/react-todo/static/js/2.de96f8a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rashidtruba/react-todo/static/js/2.de96f8a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90a37e2c87abcf3f9455",
+    "url": "/rashidtruba/react-todo/static/js/main.498689e2.chunk.js"
+  },
+  {
+    "revision": "4d580664ae8c73ce417e",
+    "url": "/rashidtruba/react-todo/static/js/runtime-main.ded118b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rashidtruba/react-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
